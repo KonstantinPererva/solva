d5a9596e74f03536b9c524deab08e8d85b282461
Kostya <pererva.kostya@outlook.com>
2020-10-13 18:23:27 +0300

    сделал бегущую строку

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,60 @@
+function Marquee (node, opt)
+{
+    var self = this;
+    self.node = node;
+    self.text = self.node.querySelectorAll('[data-text-move]');
+    self.textSingle = self.text[0];
+    self.width = self.textSingle.getBoundingClientRect().right - self.textSingle.getBoundingClientRect().left;
+    self.opt = opt;
+    self.distance = 100;
+    self.time = 1000;
+
+    self.option = Object.assign(
+        {
+                    velocity: 1
+                }
+                ,
+                    self.opt
+    );
+
+    for (let i = 0; i < self.text.length; i++)
+    {
+        self.text[i].style.animationDuration = self.width / (self.distance * self.option.velocity) * self.time + 'ms';
+    }
+}
+
 window.addEventListener('load', function ()
     {
+        var moveBox = document.querySelectorAll('.info-box-title_move-left');
+
+        if (moveBox.length)
+        {
+            var moveBoxItems = [].slice.call(document.querySelectorAll('.info-box-title_move-left'));
+
+            for (let i = 0; i < moveBoxItems.length; i++)
+            {
+                new Marquee(moveBoxItems[i],
+                    {
+                        velocity: .5
+                    })
+            }
+        }
+
+        var moveB = document.querySelectorAll('[data-node-text-move]');
+
+        if (moveB.length)
+        {
+            var moveBItems = [].slice.call(document.querySelectorAll('[data-node-text-move]'));
+
+            for (let i = 0; i < moveBItems.length; i++)
+            {
+                new Marquee(moveBItems[i],
+                    {
+                        velocity: .5
+                    })
+            }
+        }
+
         var btnToggle = document.querySelector('.button-toggle');
         var menu = document.querySelector('.menu');
         var popup = document.querySelector('.popup');
